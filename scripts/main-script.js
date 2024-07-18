c81f99aefd60b7173f21efcd5257b9bfca5f0e1d
Avi <93165148+avi4h@users.noreply.github.com>
2024-07-18 16:25:38 +0530

    correct minor

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,50 +1,57 @@
 const baseUrl = "https://www.omdbapi.com/?&apikey=370610da"
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const searchInput = document.getElementById("s-input")
-    const suggestionsContainer = document.getElementById("suggestions")
-    const btnEl = document.getElementById("s-btn")
-    const mainEl = document.getElementById("main")
+const searchInput = document.getElementById("s-input")
+const suggestionsContainer = document.getElementById("suggestions")
+const btnEl = document.getElementById("s-btn")
+const mainEl = document.getElementById("main")
 
-    searchInput.addEventListener("input", async () => {
-        const query = searchInput.value
-        if (query.length > 0) {
-            const suggestions = await fetchSuggestions(query)
-            displaySuggestions(suggestions.slice(0, 5))
-        } else {
-            suggestionsContainer.style.display = "none"
-        }
-    });
-
-    btnEl.addEventListener("click", () => {
-        const query = searchInput.value
-        fetchMovies(query)
+searchInput.addEventListener("input", async () => {
+    const query = searchInput.value
+    if (query.length > 0) {
+        const suggestions = await fetchSuggestions(query)
+        displaySuggestions(suggestions.slice(0, 5))
+    } else {
         suggestionsContainer.style.display = "none"
-    });
-
-    async function fetchSuggestions(query) {
-        const response = await fetch(`${baseUrl}&s=${query}`)
-        const data = await response.json()
-        return data.Search || []
     }
+})
 
-    async function fetchMovies(query) {
-        const response = await fetch(`${baseUrl}&s=${query}`)
-        const data = await response.json()
-        if (data.Response === "False") {
-            mainEl.innerHTML = `<h3>Unable to find what you’re looking for. Please try another search</h2>`
-            return
-        }
-        else{
-            const movies = data.Search
-            
-            const htmlMovies = await Promise.all(movies.map(async (movie) => {
-                const id = movie.imdbID
-                const res = await fetch(`${baseUrl}&i=${id}`)
-                const mov = await res.json()
-    
-                return `
+btnEl.addEventListener("click", () => {
+    const query = searchInput.value
+    fetchMovies(query)
+    suggestionsContainer.style.display = "none"
+})
+
+suggestionsContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("suggestion-item")) {
+        searchInput.value = e.target.textContent
+        suggestionsContainer.style.display = "none"
+        fetchMovies(e.target.textContent)
+    }
+})
+
+async function fetchSuggestions(query) {
+    const response = await fetch(`${baseUrl}&s=${query}`)
+    const data = await response.json()
+    return data.Search || []
+}
+
+async function fetchMovies(query) {
+    const response = await fetch(`${baseUrl}&s=${query}`)
+    const data = await response.json()
+    if (data.Response === "False") {
+        mainEl.innerHTML = `<h3>Unable to find what you’re looking for. Please try another search</h2>`
+        return
+    }
+    else {
+        const movies = data.Search
+
+        const htmlMovies = await Promise.all(movies.map(async (movie) => {
+            const id = movie.imdbID
+            const res = await fetch(`${baseUrl}&i=${id}`)
+            const mov = await res.json()
+
+            return `
                     <div class="movie">
                         <div class="div1">
                             <img src="${mov.Poster}" alt="${mov.Title}">
@@ -55,33 +62,27 @@ document.addEventListener("DOMContentLoaded", () => {
                             <p>${mov.Plot}</p>
                         </div>
                     </div>`
-            }))
-    
-            mainEl.innerHTML = htmlMovies.join("")
-        }
-    }
+        }))
 
-    function displaySuggestions(suggestions) {
-        suggestionsContainer.innerHTML = "";
-        if(suggestions.length > 0) {
-            suggestions.forEach(suggestion => {
-                const suggestionItem = document.createElement("div")
-                suggestionItem.classList.add("suggestion-item")
-                suggestionItem.textContent = suggestion.Title
-                suggestionsContainer.appendChild(suggestionItem)
-            });
-            suggestionsContainer.style.display = "block"
-        } 
-        else {
-            suggestionsContainer.style.display = "none"
-        }
+        mainEl.innerHTML = htmlMovies.join("")
     }
+}
 
-    suggestionsContainer.addEventListener("click", (e) => {
-        if (e.target.classList.contains("suggestion-item")) {
-            searchInput.value = e.target.textContent
-            suggestionsContainer.style.display = "none"
-            fetchMovies(e.target.textContent)
-        }
-    })
-})
+function displaySuggestions(suggestions) {
+    suggestionsContainer.innerHTML = "";
+    if (suggestions.length > 0) {
+        suggestions.forEach(suggestion => {
+            const suggestionItem = document.createElement("div")
+            suggestionItem.classList.add("suggestion-item")
+            suggestionItem.textContent = suggestion.Title
+            suggestionsContainer.appendChild(suggestionItem)
+        });
+        suggestionsContainer.style.display = "block"
+    }
+    else {
+        suggestionsContainer.style.display = "none"
+    }
+}
+
+
+
